7f5966bfd3eebdb128e7136d4960eec31d908915
Vukay <ret.gmail.com>
2025-12-23 19:29:27 +0600

    создал view cars_views и url cars/

--- django1/urls.py
+++ django1/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from core.views import index_views, ret
+from core.views import index_views, cars_views, ret
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_views),
+    path('cars/', cars_views),
     path('ret/', ret)
 ]
